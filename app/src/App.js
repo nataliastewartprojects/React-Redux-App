8b62956adf7e735f11300106474ad4163757b7cc
Natalia <nataliamirandac@hotmail.com>
2020-05-13 18:19:35 -0400

    CovidPage was displayed on the browser

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import CovidPage from "./components/CovidPage";
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="App">
+      <CovidPage />
+    </div>
+  );
 }
 
 export default App;
--- app/src/components/CovidPage.js
+++ app/src/components/CovidPage.js
@@ -28,4 +28,4 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(CovidPage, {})(CovidPage);
+export default connect(mapStateToProps, {})(CovidPage);
